72910fd007df3c99d2b7c57b187ff5c6f7a92f29
Francisco Florio <fflorio@algeiba.com>
2022-12-07 11:52:05 -0300

    App Returning its version

---
+++ ALG_Demo_fflorio_SemVer/Route/AppVersionRoute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ALG_Demo_fflorio_SemVer.Route
+{
+    public static class AppVersionRoute
+    {
+        public static IEndpointRouteBuilder MapVersions(this IEndpointRouteBuilder builder)
+        {
+            builder.MapGet("/appVersion", () =>
+            {
+                var AppVersion = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("AppVersion")["Version"];
+                
+                return AppVersion;
+            })
+            .WithName("GetVersion");
+
+            return builder;
+        }
+    }
+}
