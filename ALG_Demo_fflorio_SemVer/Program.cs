3929e51d0d58d5353c0fdfa962775c3dcf089998
Francisco Florio <fflorio@algeiba.com>
2022-12-12 11:52:14 -0300

    Ups

--- ALG_Demo_fflorio_SemVer/Program.cs
+++ ALG_Demo_fflorio_SemVer/Program.cs
@@ -19,11 +19,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(); 
 }
 
 app.UseHttpsRedirection();
 
-app.MapControllers();
 
 app.Run();
